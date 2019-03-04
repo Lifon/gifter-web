4908e18c6fcfea1c7d5ce11bce82580ea2c47fac
lipon <lifo.info@gmail.com>
2019-03-04 19:39:02 +0600

    Test project is created for gifter web

---
+++ models/Department.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "department".
+ *
+ * @property string $id
+ * @property string $name
+ *
+ * @property Employee[] $employees
+ */
+class Department extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'department';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEmployees()
+    {
+        return $this->hasMany(Employee::className(), ['dep_id' => 'id']);
+    }
+}
